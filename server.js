e8b835b00d73ec283c3542805a0e4f354b340681
MohnadBabgi <mohanadbabgi@gmail.com>
2025-09-19 19:53:34 +0300

    Adding images

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 import http from 'http'
 import fs from 'fs/promises'
-import { serveStatic } from './utils/ServeStatic.js';
-import { sendJSONres } from './utils/sendResponse.js';
+import { serveStatic } from './utils/ServeStatic.js'
+import { sendJSONres } from './utils/sendResponse.js'
 import path from 'path'
 const __dirname = import.meta.dirname
 
 const PORT = 8000;
 
-  const server = http.createServer( async(req, res) => {
-  const pathToResource = path.join(__dirname, 'public', 'index.html')
+  const server = http.createServer(async(req, res) => {
+  const publicDir = path.join(__dirname, 'public')
+  const pathToResource = path.join(publicDir, req.url === '/' ? 'index.html' : req.url)
   const content = await fs.readFile(pathToResource,'utf8')
-  res.statusCode = 200;
-  sendJSONres(res,res.statusCode,content)
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end(content)
   
 });
 
-server.listen(PORT, () => console.log(`Connected on Port: ${PORT}`));
+server.listen(PORT, () => console.log(`Connected on Port: ${PORT}`))
